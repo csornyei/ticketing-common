502369b2990b54acdd525c9fe4c74a4806238d5b
matecsornyei <mate@csornyei.com>
2021-03-24 21:36:27 +0100

    Event publishing and listening

---
+++ src/events/index.ts
@@ -0,0 +1,6 @@
+export * from './TicketEvents/ticket-created-event';
+export * from './TicketEvents/ticket-updated-event';
+
+export * from './base-listener';
+export * from './base-publisher';
+export * from './subjects';

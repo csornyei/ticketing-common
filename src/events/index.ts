75b7057a1b641b23a59f77877054215ed8cce8e8
matecsornyei <mate@csornyei.com>
2021-03-25 13:32:44 +0100

    Order status added

--- src/events/index.ts
+++ src/events/index.ts
@@ -1,6 +1,8 @@
 export * from './TicketEvents/ticket-created-event';
 export * from './TicketEvents/ticket-updated-event';
 
+export * from './types/order-status';
+
 export * from './base-listener';
 export * from './base-publisher';
 export * from './subjects';

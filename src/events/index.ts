d9f8fe1964478254b2aea6910ec825cd1fd356b3
matecsornyei <mate@csornyei.com>
2021-03-28 20:52:28 +0200

    Expiration complete event

--- src/events/index.ts
+++ src/events/index.ts
@@ -2,6 +2,7 @@ export * from './TicketEvents/ticket-created';
 export * from './TicketEvents/ticket-updated';
 export * from './OrderEvents/order-created'
 export * from './OrderEvents/order-cancelled';
+export * from './OrderEvents/expiration-complete';
 
 export * from './types/order-status';
 
